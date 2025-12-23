4a27137d7d6bfc2dcfcf2a145a8e90409ab3dd3d
BennyBoy10 <folamiemmanuel18@gmail.com>
2025-12-23 18:12:04 +0100

    Initial commit

---
+++ 1.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
+
+echo ""
+echo -e "\e[32mStarting... \e[0m"
+echo ""
+
+git add .
+git commit -m "Update BennyBoy10"
+git push origin main
+
+echo ""
+echo -e "\e[32mDone!! \e[0m"
+echo ""
