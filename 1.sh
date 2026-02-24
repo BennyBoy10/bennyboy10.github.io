8567f55ae5e6a757658fdeb5f979c7f621997eed
BennyBoy10 <folamiemmanuel18@gmail.com>
2026-02-24 18:54:24 +0100

    Updated BennyBoy10

--- 1.sh
+++ 1.sh
@@ -6,10 +6,9 @@ echo -e "\e[32mStarting... \e[0m"
 echo ""
 
 git add .
-git commit -m "Update BennyBoy10"
+git commit -m "Updated BennyBoy10"
 git push origin main
 
 echo ""
 echo -e "\e[32mDone!! \e[0m"
 echo ""
-
